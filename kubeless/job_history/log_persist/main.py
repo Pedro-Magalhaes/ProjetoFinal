cc317d5ee6113c0cfaa3e88513e87088cc0a88ae
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2020-07-30 22:54:01 -0300

    Corrigindo arquivo na pasta incorreta

--- kubeless/job_history/log_persist/main.py
+++ kubeless/job_history/log_persist/main.py
@@ -1,34 +1,22 @@
-import base64
+
 import json
 import os
-from google.cloud import storage
      
 
 def hello_pubsub(event, context):
      
      if event:
           print(event)
-          #pubsub_message = base64.b64decode(event).decode('utf-8')
-          pubsub_message = json.dumps(event) + '\n'
-          print(pubsub_message)
-          bucket = os.environ.get('output_bucket')
-          print("Using bucket: " + bucket)
-          storage_client = storage.Client()
-          bucket = storage_client.bucket(bucket)
-
-          # Blob que vai  manter o log consolidado
-          log_blob_name = os.environ.get('log_blob')
-          log_blob = bucket.blob(log_blob_name)
           
-          # Blob com a nova consulta
-          new_blob = bucket.blob(event['id'])
-          new_blob.upload_from_string(pubsub_message)
+          pubsub_message = json.dumps(event['data']) + '\n'
+          print(pubsub_message)
+          path = '/data/pv01/'
+          print("Using folder: " + path)
+          log_file_name = path + 'log.txt'
 
-          # consolidando os blobs (nao eh posivel fazer append)
-          log_blob.compose([log_blob,new_blob])
-
-          # deletando o que foi adicionado ao log
-          new_blob.delete()
+          # Mais simples que o google, MAS não eh safe pra acesso concorrente!!!
+          with open(log_file_name,'a') as log_file:
+               log_file.write(pubsub_message)
 
      else:
           print("Erro sem data no event")
